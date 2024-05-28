46d19a77cad4fc9b3155cdbb9d5dca4957aab4bc
maria clara bassedon <118577937+bassedon041@users.noreply.github.com>
2024-05-28 19:57:11 -0300

    link

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ const caixaPerguntas = document.querySelector ('.caixa-perguntas');
 const caixaAlternativas = document.querySelector ('.caixa-alternativas');
 const caixaResultado = document.querySelector ('.caixa-resultado');
 const textoResultado = document.querySelector ('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternatina 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "alternatina 1",
+            "alternativa 2"
+        ]
+    },
+    
+    {
+        enunciado: "pergunta 3",
+        alternativas: [
+            "alternatina 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 4",
+        alternativas: [
+            "alternatina 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 5",
+        alternativas: [
+            "alternatina 1",
+            "alternativa 2"
+        ]
+    }
+];
